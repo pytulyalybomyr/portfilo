7dffcbfba78237b56e9fad16f49d82944e73bff2
pytulyalybomyr <pytulyalybomyr@gmail.com>
2023-09-13 15:28:38 +0300

    3-commit

---
+++ js/burger.js
@@ -0,0 +1,17 @@
+let burger_open = document.querySelector('.burger-open')
+let burger_close = document.querySelector('#burger-close')
+let burger = document.querySelector('.header-burger__main')
+
+burger_open.addEventListener('click', () => {
+    burger.classList.toggle("hide")
+
+    console.log("Dfdfdf")
+
+})
+
+burger_close.addEventListener('click', () => {
+    burger.classList.toggle("hide")
+
+    console.log("Dfdfdf")
+
+})
