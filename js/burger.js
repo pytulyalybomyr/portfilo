6d53d41d4357315c531f70d693fa391c883e535f
pytulyalybomyr <pytulyalybomyr@gmail.com>
2023-09-14 22:10:16 +0300

    first commit

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,9 @@
-let burger_open = document.querySelector('.burger-open')
-let burger_close = document.querySelector('#burger-close')
-let burger = document.querySelector('.header-burger__main')
+$('.burger-close').click(function () {
+    $('.header-burger__main').addClass("hide")
+    console.log("Sasasas")
+});
 
-burger_open.addEventListener('click', () => {
-    burger.classList.toggle("hide")
+$('.burger-open').click(function () {
+    $('.header-burger__main').removeClass('hide');
 
-    console.log("Dfdfdf")
-
-})
-
-burger_close.addEventListener('click', () => {
-    burger.classList.toggle("hide")
-
-    console.log("Dfdfdf")
-
-})
+});
