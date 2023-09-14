155d27209e5f9ce777535d8d96d1b5aa7183b5d6
pytulyalybomyr <pytulyalybomyr@gmail.com>
2023-09-14 22:47:32 +0300

    2-commit

--- js/burger.js
+++ js/burger.js
@@ -7,3 +7,32 @@ $('.burger-open').click(function () {
     $('.header-burger__main').removeClass('hide');
 
 });
+
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+var b_proj = document.querySelector('.b-proj');
+var proj = document.querySelector('#proj');
+
+var b_home = document.querySelector('.b-home');
+var home = document.querySelector('#home');
+
+var b_contact = document.querySelector('.b-contact');
+var contact = document.querySelector('#contact');
+
+b_proj.addEventListener('click', () => {
+    scrollTo(proj);
+})
+
+b_home.addEventListener('click', () => {
+    scrollTo(home);
+})
+
+b_contact.addEventListener('click', () => {
+    scrollTo(contact);
+})
